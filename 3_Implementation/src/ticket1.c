20aba8351cd1e29774b5b7a89d07b158f91097d9
SoundaryalakshmiSaravanan <98818008+SoundaryalakshmiSaravanan@users.noreply.github.com>
2022-02-11 18:47:28 +0530

    Update ticket1.c

--- 3_Implementation/src/ticket1.c
+++ 3_Implementation/src/ticket1.c
@@ -10,10 +10,10 @@ void show1(int preference,char name[10],int id1,int amount)
 		printf("\n\n");
         //printf("\t-----------------THEATER BOOKING TICKET----------------\n");
         printf("\t******************\n");
-        printf("\t Booking ID : %d \t\t\tShow Name : Harry Potter-7\n",id1);
+        printf("\t Booking ID : %d \t\t\tShow Name : Harry Potter\n",id1);
         printf("\t Customer  : %s\n",name);
-        printf("\t\t\t                              Date      : 07-05-2019\n");
-        printf("\t                                              Time      : 07:00pm\n");
+        printf("\t\t\t                              Date      : 11-02-2022\n");
+        printf("\t                                              Time      : 08:00am\n");
         printf("\t                                              Hall      : 02\n");
         printf("\t                                              seats No. : %d  \n",preference);
         printf("\t                                              price . : %d  \n\n",amount);
