db0504dd2d78c88ea08b502cb5e4de4bf1b93543
SoundaryalakshmiSaravanan <98818008+SoundaryalakshmiSaravanan@users.noreply.github.com>
2022-02-11 20:46:49 +0530

    Update movie.c

--- 3_Implementation/src/movie.c
+++ 3_Implementation/src/movie.c
@@ -6,9 +6,9 @@ int ListofMovies(void)
 	system("cls");
 	printf("\t\t\tMovie Name?\n");
 	printf("\t\t\t----------------------------\n\n");
-	printf("\t\t\tpress 1 for Harry Potter\n\n");
-	printf("\t\t\tpress 2 for The Invisible Man\n\n");
-	printf("\t\t\tpress 3 for Tenet\n");
+	printf("\t\t\tpress 1 for Titanic\n\n");
+	printf("\t\t\tpress 2 for No time To Die\n\n");
+	printf("\t\t\tpress 3 for Encanto\n");
 	scanf("%d",&i);
 	return i;
 }
