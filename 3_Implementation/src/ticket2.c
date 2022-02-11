4ebc208bc33a1e7bd377d91f466370db2a639cde
SoundaryalakshmiSaravanan <98818008+SoundaryalakshmiSaravanan@users.noreply.github.com>
2022-02-11 20:36:49 +0530

    Update ticket2.c

--- 3_Implementation/src/ticket2.c
+++ 3_Implementation/src/ticket2.c
@@ -10,7 +10,7 @@ void show2(int preference,char name[10],int id1,int amount)
 		printf("\n\n");
        
         printf("\t============================================================\n");
-        printf("\t Booking ID : %d \t\t\tShow Name :The Invisible Man\n",id1);
+        printf("\t Booking ID : %d \t\t\tShow Name :No Time To Die\n",id1);
         printf("\t Customer  : %s\n",name);
         printf("\t\t\t                                Date      : 11-02-2022\n");
         printf("\t                                              Time      : 09:00am\n");
