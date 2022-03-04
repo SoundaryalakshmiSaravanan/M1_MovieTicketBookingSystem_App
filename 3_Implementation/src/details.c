b93be933dcdf9faec1dd2ef89ebe9db5ba592ecb
SoundaryalakshmiSaravanan <98818008+SoundaryalakshmiSaravanan@users.noreply.github.com>
2022-03-04 16:42:53 +0530

    Added implementation to milestone2, #2

--- 3_Implementation/src/details.c
+++ 3_Implementation/src/details.c
@@ -1,8 +1,18 @@
+/**
+ * @file function.h
+ * @author SoundaryalakshmiSaravanan (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include "function.h"
 #include "moviedetails.c"
 #include<string.h>
 
-struct movie_details person[300];
+struct movie_details person[300];     //Structure to hold ticket details
 void details(void)
 {
 	int i;
@@ -15,6 +25,7 @@ void details(void)
 		for (i=0;i<count;i++)
 		{
 			printf("seat no: %d is booked by %s booking id is %d\n",person[i].placenum,person[i].name,person[i].id);
+			//Ticket booked along with seat number,name, booking id is displayed
 		}
 	}
 	else
