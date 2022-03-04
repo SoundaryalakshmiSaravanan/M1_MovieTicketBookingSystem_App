dbefceb56a1901d50e4abb83102bdfb5b36662fa
SoundaryalakshmiSaravanan <98818008+SoundaryalakshmiSaravanan@users.noreply.github.com>
2022-03-04 16:49:28 +0530

    Added implementation to milestone2, #2

--- 3_Implementation/src/price.c
+++ 3_Implementation/src/price.c
@@ -1,12 +1,22 @@
+/**
+ * @file function.h
+ * @author SoundaryalakshmiSaravanan (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include "function.h"
-int changeamountticket(int amount)
+int changeamountticket(int amount)       // Function to change amount can be done only by admin
 {
 	char pass[10],pak[10]="admin";
 	printf("Enter the password to change price of ticket: ");
 	scanf("%s",&pass);
 	if (strcmp(pass,pak)==0)
 	{
-		printf("Please enter new price: ");
+		printf("Please enter new price: ");      
 		scanf("%d",&amount);
 		printf("price Updated Successfully: ");
 		system("PAUSE");
