be36e0b43a41e744b05814e1c0c9e82bb8811c6b
SoundaryalakshmiSaravanan <98818008+SoundaryalakshmiSaravanan@users.noreply.github.com>
2022-03-04 16:56:56 +0530

    Update tic1.c

--- 3_Implementation/src/tic1.c
+++ 3_Implementation/src/tic1.c
@@ -1,3 +1,13 @@
+/**
+ * @file function.h
+ * @author SoundaryalakshmiSaravanan (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include "function.h"
 #include "moviedetails.c"
 #include<string.h>
