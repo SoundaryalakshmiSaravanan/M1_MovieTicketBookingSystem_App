0713ca30802e901dfa9b6e760bad4404a475bd68
SoundaryalakshmiSaravanan <98818008+SoundaryalakshmiSaravanan@users.noreply.github.com>
2022-03-04 16:34:44 +0530

    Added implementation to milestone2, #2

--- 3_Implementation/src/choice1.c
+++ 3_Implementation/src/choice1.c
@@ -1,7 +1,17 @@
+/**
+ * @file function.h
+ * @author SoundaryalakshmiSaravanan (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include "function.h"
 int enterpreference(void)
 {
-	int preference;
+	int preference;     // variable to store the prefered seat by the user
 	printf("                 Welcome To Movie Ticket Booking System\n");
 	printf(" ************#######************\n");
 	printf("             1- Admin           \n");
