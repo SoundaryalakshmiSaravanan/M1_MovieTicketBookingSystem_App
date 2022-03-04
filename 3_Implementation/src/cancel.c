919d8e8d8c6db122f2930f4b9c3fe938e359559d
SoundaryalakshmiSaravanan <98818008+SoundaryalakshmiSaravanan@users.noreply.github.com>
2022-03-04 16:32:04 +0530

    Added implementation to milestone2, #2

--- 3_Implementation/src/cancel.c
+++ 3_Implementation/src/cancel.c
@@ -1,11 +1,22 @@
+/**
+ * @file function.h
+ * @author SoundaryalakshmiSaravanan (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include "function.h"
 #include "moviedetails.c"
 #include<string.h>
 
 struct movie_details person[300];
-void cancelticket(int *array)
+void cancelticket(int *array)              
+// Function to cancel the booked tickets
 {
-      int Cseat,i,stop;
+      int Cseat,i,stop;                    
 	  printf("Please enter ID number of ticket: ");
 	  scanf("%d",&Cseat);
 	  for (i=0;i<300;i++)
@@ -14,11 +25,13 @@ void cancelticket(int *array)
 	  		{
 					 stop=5;
 					 system("cls");
+				// If condition satisifies the below is shown
 					 printf("%s your ticket is %d cancelled",person[i].name,person[i].placenum);
 					 array[person[i].placenum]=0;
 					 i=300;
 	  		}
 	  }
-	  if (stop!=5)
+	  if (stop!=5)                   
+          //If the ticket id is given wrong the below statement is shown
 	  		printf("Ticket ID number is incorrect please enter right one to cancel ticket: \n");
 }
