a9d5d86486b1b23a7d73f17f79301dc5a5bfc31f
SoundaryalakshmiSaravanan <98818008+SoundaryalakshmiSaravanan@users.noreply.github.com>
2022-03-04 16:46:59 +0530

    Added implementation to milestone2, #2

--- 3_Implementation/src/moviedetails.c
+++ 3_Implementation/src/moviedetails.c
@@ -1,8 +1,18 @@
+/**
+ * @file function.h
+ * @author SoundaryalakshmiSaravanan (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include<stdio.h>
 
 struct movie_details{
-	char name[25];
-	char phoneno[15];
-	int seatnum;
-	int id;
+	char name[25];    // variable to hold name
+	char phoneno[15]; // variable to hold phone number
+	int seatnum;      // variable to hold seat number
+	int id;           // variable to hold booking Id
 };
